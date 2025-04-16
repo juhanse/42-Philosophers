1b69fae7b986ad30dea0821e249b97d3ed29d058
juhanse <julien.hanse12@gmail.com>
2025-04-16 23:23:28 +0200

    add: init_philo

--- srcs/main.c
+++ srcs/main.c
@@ -6,32 +6,38 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 18:26:15 by juhanse           #+#    #+#             */
-/*   Updated: 2025/04/15 20:01:14 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/04/16 23:22:24 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	ft_init(t_philo *philo, char **argv, int hasTimer)
+int	ft_init_philo(t_philo *philo, char **argv)
 {
-	*philo = (t_philo){0};
+	if (!argv[1] || !argv[2] || !argv[3] || !argv[4])
+		return (1);
 	philo->data->nb_philos = ft_atoi(argv[1]);
 	philo->data->time_to_die = ft_atoi(argv[2]);
 	philo->data->time_to_eat = ft_atoi(argv[3]);
 	philo->data->time_to_sleep = ft_atoi(argv[4]);
-	if (hasTimer)
+	if (philo->data->nb_philos < 1 || philo->data->nb_philos > 200
+		|| philo->data->time_to_die < 0 || philo->data->time_to_eat < 0
+		|| philo->data->time_to_sleep < 0)
+		return (1);
+	if (philo->data->nb_philos == 1)
+		return (printf("0 1 has taken a fork\n"), printf("0 1 has died\n"), 1);
+	if (argv[5])
 		philo->data->num_of_eating = ft_atoi(argv[5]);
+	return (0);
 }
 
 int	main(int argc, char **argv)
 {
 	t_philo	philo;
 
-	if (argc == 4)
-		ft_init(&philo, argv, 0);
-	else if (argc == 5)
-		ft_init(&philo, argv, 1);
-	else
-		printf("Invalid arguments\n");
+	if (argc != 5 && argc != 6)
+		return (printf("not the mount of args asked for, argc : %d\n", argc), 1);
+	if (ft_init_philo(&philo, argv) == 1)
+		return (1);
 	return (0);
 }
