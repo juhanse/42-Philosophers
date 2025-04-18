ddcb3e3c93df51fcce415dd8befd1f4f0d2211c3
juhanse <julien.hanse12@gmail.com>
2025-04-18 16:20:28 +0200

    fix: check args

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 18:26:15 by juhanse           #+#    #+#             */
-/*   Updated: 2025/04/17 16:18:14 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/04/18 16:16:40 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ int	main(int argc, char **argv)
 	t_data	data;
 
 	if (argc != 5 && argc != 6)
-		return (printf("not the mount of args asked for, argc : %d\n", argc), 1);
+	{
+		printf("wrong number of args: %d\n", argc);
+		return (1);
+	}
 	if (init_data(&data, argv))
 		return (1);
 	return (0);
