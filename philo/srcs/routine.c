90dd1c38bdb2931a1ffcf4364cb93da8dae9fe4b
juhanse <julien.hanse12@gmail.com>
2025-04-22 12:03:55 +0200

    add: create_thead function

--- philo/srcs/routine.c
+++ philo/srcs/routine.c
@@ -6,14 +6,31 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 11:54:09 by juhanse           #+#    #+#             */
-/*   Updated: 2025/04/22 11:56:13 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/04/22 12:02:04 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
 
+int	ft_create_threads(t_data *data)
+{
+	int	i;
+
+	data->start_time = get_time();
+	i = -1;
+	while (++i < data->nb_philos)
+	{
+		data->philos[i].last_meal = data->start_time;
+		// if (pthread_create(&data->philos[i].thread, NULL, &philo_routine, &data->philos[i]))
+		// 	return (1);
+		usleep(100);
+	}
+	return (0);
+}
+
 int	ft_routine(t_data *data)
 {
-	ft_debug(data);
+	if (ft_create_threads(data))
+		return (1);
 	return (0);
 }
