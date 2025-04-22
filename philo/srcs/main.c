2221f45746d482e1d219068c76862e9fcc1c03e4
juhanse <julien.hanse12@gmail.com>
2025-04-22 11:48:45 +0200

    add: debug function

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,11 +6,20 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 18:26:15 by juhanse           #+#    #+#             */
-/*   Updated: 2025/04/22 11:30:48 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/04/22 11:45:38 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
+
+void	ft_debug(t_data *data)
+{
+	printf("nb_philos: %d\n", data->nb_philos);
+	printf("time_to_die: %d\n", data->time_to_die);
+	printf("time_to_eat: %d\n", data->time_to_eat);
+	printf("time_to_sleep: %d\n", data->time_to_sleep);
+	printf("num_of_eating: %d\n", data->num_of_eating);
+}
 
 int	main(int argc, char **argv)
 {
@@ -20,5 +29,6 @@ int	main(int argc, char **argv)
 		return (printf(ERR_ARGS), 1);
 	if (ft_init_data(&data, argv))
 		return (1); 
+	ft_debug(&data);
 	return (0);
 }
