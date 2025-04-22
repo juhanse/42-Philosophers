c4424ec8f426c9d2518cdfa34b137f1c985fb7be
juhanse <julien.hanse12@gmail.com>
2025-04-22 12:31:02 +0200

    fix: pthreads create routine

--- philo/srcs/routine.c
+++ philo/srcs/routine.c
@@ -6,13 +6,18 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 11:54:09 by juhanse           #+#    #+#             */
-/*   Updated: 2025/04/22 12:02:04 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/04/22 12:29:41 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
 
-int	ft_create_threads(t_data *data)
+void	*ft_threads()
+{
+	printf("TEST\n");
+}
+
+int	ft_routine(t_data *data)
 {
 	int	i;
 
@@ -21,16 +26,11 @@ int	ft_create_threads(t_data *data)
 	while (++i < data->nb_philos)
 	{
 		data->philos[i].last_meal = data->start_time;
-		// if (pthread_create(&data->philos[i].thread, NULL, &philo_routine, &data->philos[i]))
-		// 	return (1);
-		usleep(100);
+		if (pthread_create(&data->philos[i].thread, NULL, &ft_threads, &data->philos[i]))
+		{
+			return (1);
+		}
 	}
-	return (0);
-}
-
-int	ft_routine(t_data *data)
-{
-	if (ft_create_threads(data))
-		return (1);
+	ft_destroy_mutex(data);
 	return (0);
 }
