28b3426b3a530f80564b4bc84a64b00640146502
juhanse <julien.hanse12@gmail.com>
2025-04-23 22:53:13 +0200

    add: parsing

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,18 +6,36 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 18:26:15 by juhanse           #+#    #+#             */
-/*   Updated: 2025/04/23 21:45:29 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/04/23 22:52:24 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
 
+static int	ft_parsing(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	if (ac != 5 && ac != 6)
+		return (printf(ERR_ARGS), 1);
+	i = 0;
+	while (av[++i])
+	{
+		j = -1;
+		while (av[i][++j])
+			if (!ft_isdigit(av[i][j]))
+				return (printf(ERR_ARGS), 1);
+	}
+	return (0);
+}
+
 int	main(int argc, char **argv)
 {
 	t_data	data;
 
-	if (argc != 5 && argc != 6)
-		return (printf(ERR_ARGS), 1);
+	if (ft_parsing(argc, argv))
+		return (1);
 	data = (t_data){0};
 	if (ft_init_data(&data, argv))
 		return (1);
