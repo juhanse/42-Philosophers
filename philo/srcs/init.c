90d98bbda13ea34afb19f39715a79f465cc4d796
juhanse <julien.hanse12@gmail.com>
2025-05-07 15:57:46 +0200

    fix: init forks

--- philo/srcs/init.c
+++ philo/srcs/init.c
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 15:40:17 by juhanse           #+#    #+#             */
-/*   Updated: 2025/05/07 12:26:39 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/05/07 15:52:37 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,17 @@
 
 int	ft_init_mutex(t_data *data)
 {
+	int	i;
+
+	i = -1;
+	while (++i < data->nb_philos)
+		if (pthread_mutex_init(&data->forks[i], NULL))
+			return (1);
 	if (pthread_mutex_init(&data->m_print, NULL))
 		return (1);
 	if (pthread_mutex_init(&data->m_eat, NULL))
 		return (1);
 	if (pthread_mutex_init(&data->m_stop, NULL))
-		return (1);
-	if (pthread_mutex_init(&data->m_dead, NULL))
 		return (1);
 	return (0);
 }
@@ -33,20 +37,16 @@ int	ft_init_philo(t_data *data)
 	if (ft_init_mutex(data))
 		return (1);
 	data->t_start = ft_get_time();
+	i = -1;
 	while (++i < data->nb_philos)
 	{
 		data->philo[i].id = i + 1;
-		data->philo[i].last_eat = data->t_start;
-		data->philo[i].fork_right = NULL;
 		data->philo[i].data = data;
+		data->philo[i].last_eat = data->t_start;
 		data->philo[i].times_eaten = 0;
-		pthread_mutex_init(&(data->philo[i].fork_left), NULL);
-		if (i == data->nb_philos - 1)
-			data->philo[i].fork_right = &data->philo[0].fork_left;
-		else
-			data->philo[i].fork_right = &data->philo[i + 1].fork_left;
-		if (pthread_create(&data->philo[i].thread, NULL, ft_philo, \
-			(void *)&data->philo[i]) != 0)
+		data->philo[i].fork_left = &data->forks[i];
+		data->philo[i].fork_right = &data->forks[(i + 1) % data->nb_philos];
+		if (pthread_create(&data->philo[i].thread, NULL, ft_philo, &data->philo[i]) != 0)
 			return (ft_free(data), 1);
 		data->nb_threads++;
 	}
@@ -69,8 +69,9 @@ int	ft_init_data(t_data *data, char **av)
 	if (data->t_die < 0 || data->t_eat < 0
 		|| data->t_sleep < 0 || (av[5] && data->n_eat < 1))
 		return (printf(ERR_ARGS), 1);
-	data->philo = malloc(sizeof(t_philo) * data->nb_philos);
-	if (!data->philo)
-		return (printf(ERR_MALLOC), 1);
+	if (!(data->philo = malloc(sizeof(t_philo) * data->nb_philos)))
+		return (ERR_MALLOC, 1);
+	if (!(data->forks = malloc(sizeof(pthread_mutex_t) * data->nb_philos)))
+		return (free(data->philo), ERR_MALLOC, 1);
 	return (0);
 }
