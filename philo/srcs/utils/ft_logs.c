2a9aa61b853692da7afc8b097f6173a1d76c058c
juhanse <julien.hanse12@gmail.com>
2025-05-08 17:27:54 +0200

    fix: mutex logs function

--- philo/srcs/utils/ft_logs.c
+++ philo/srcs/utils/ft_logs.c
@@ -6,25 +6,19 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 12:41:19 by juhanse           #+#    #+#             */
-/*   Updated: 2025/05/06 15:08:27 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/05/08 17:16:33 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../philo.h"
 
-void	ft_logs(t_data *data, int id, char *msg)
+void	ft_logs(t_philo *philo, char *msg)
 {
 	long long	time;
 
-	if (ft_should_stop(data))
-		return ;
-	pthread_mutex_lock(&data->m_print);
-	if (ft_should_stop(data))
-	{
-		pthread_mutex_unlock(&data->m_print);
-		return ;
-	}
-	time = ft_get_time();
-	printf("%lld %d %s\n", time - data->t_start, id, msg);
-	pthread_mutex_unlock(&data->m_print);
+	time = ft_get_time() - philo->data->t_start;
+	pthread_mutex_lock(&philo->data->m_print);
+	if (!ft_should_stop(philo->data))
+		printf("%lld %d %s\n", time, philo->id, msg);
+	pthread_mutex_unlock(&philo->data->m_print);
 }
